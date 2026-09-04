d63f10b95aca9d4fa109f7e30f33a9e4c569e313
Malsha Shehan <mshehan989@gmail.com>
2026-09-04 18:20:58 +0530

    Implemented the Mobile Repair and Product List UI.

--- app/src/main/java/com/example/techfix/CustomerDashboard.java
+++ app/src/main/java/com/example/techfix/CustomerDashboard.java
@@ -10,6 +10,7 @@ public class CustomerDashboard extends AppCompatActivity {
 
     Button btnComputerRepair;
     Button btnMobileRepair;
+    Button btnViewProducts;
     Button btnMyAppointments;
     Button btnRepairHistory;
     Button btnFeedback;
@@ -22,17 +23,41 @@ public class CustomerDashboard extends AppCompatActivity {
 
         btnComputerRepair = findViewById(R.id.btnComputerRepair);
         btnMobileRepair = findViewById(R.id.btnMobileRepair);
+        btnViewProducts = findViewById(R.id.btnViewProducts);
         btnMyAppointments = findViewById(R.id.btnMyAppointments);
         btnRepairHistory = findViewById(R.id.btnRepairHistory);
         btnFeedback = findViewById(R.id.btnFeedback);
 
+        // Computer Repair
         btnComputerRepair.setOnClickListener(v -> {
-            Intent intent = new Intent(CustomerDashboard.this, ComputerRepairActivity.class);
+
+            Intent intent = new Intent(
+                    CustomerDashboard.this,
+                    ComputerRepairActivity.class
+            );
+
             startActivity(intent);
         });
 
+        // Mobile Repair
         btnMobileRepair.setOnClickListener(v -> {
-            Intent intent = new Intent(CustomerDashboard.this, MobileRepairActivity.class);
+
+            Intent intent = new Intent(
+                    CustomerDashboard.this,
+                    MobileRepairActivity.class
+            );
+
+            startActivity(intent);
+        });
+
+        // View Products
+        btnViewProducts.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    CustomerDashboard.this,
+                    ProductListActivity.class
+            );
+
             startActivity(intent);
         });
     }
